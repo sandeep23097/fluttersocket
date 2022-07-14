80547df51b6cfd832a2c5a3ed0f233347a078b6a
sandeep23097 <sandeepsinghmbbs@gmail.com>
2022-07-14 16:08:59 +0530

    first commit

---
+++ webserver.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { Server } = require('ws');
+
+
+const PORT = process.env.PORT || 3000; //port for https
+
+const server = express()
+    .use((req, res) => res.send("Hello, you!"))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+
+wss.on('connection', function(ws, req) {
+    ws.on('message', message => { // If there is any message
+        var datastring = message.toString();
+        if(datastring.charAt(0) == "{"){ // Check if message starts with '{' to check if it's json
+            datastring = datastring.replace(/\'/g, '"');
+            var data = JSON.parse(datastring)
+            if(data.cmd == "newData"){
+                // TODO: Create login function
+                var loginData = '{"buffer":"'+data.buffer+'","status":"succes"}';
+                // Send data back to user
+                ws.send(loginData);
+            }
+            
+        }
+    }) 
+})
